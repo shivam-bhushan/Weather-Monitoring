b5bd8130f689062c69856e4270231a27e2c35aa2
shivam-bhushan <shivam13202@gmail.com>
2024-10-21 04:08:32 +0530

    cors2

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -3,7 +3,7 @@ import User from '@/models/userModel'
 import {NextRequest, NextResponse} from 'next/server'
 import bcryptjs from 'bcryptjs'
 import jwt from 'jsonwebtoken'
-import { setCorsHeaders } from '@/utils/cors'; // Import the CORS utility function
+import { setCorsHeaders } from '../../../utils/cors'; // Import the CORS utility function
 
 
 connect()
